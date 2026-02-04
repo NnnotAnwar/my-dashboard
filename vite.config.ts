6382e1579bb8fd073306bd6a01623c3e462fd97e
Anuar Kairulla <anwar@Mac.docsis.vodafone.cz>
2026-02-04 22:53:56 +0100

    fix pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
-      registerType: 'autoUpdate', // Магия: обновляется само в реальном времени
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'My Smart ToDo',
         short_name: 'ToDo AI',
         description: 'Умный список задач с AI и админкой',
         theme_color: '#F7F7F5',
         background_color: '#F7F7F5',
-        display: 'standalone', // Запускается без рамок браузера
+        display: 'standalone',
         icons: [
           {
             src: 'pwa-192x192.png',
@@ -24,9 +28,15 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
     })
-  ]
+  ],
 })
